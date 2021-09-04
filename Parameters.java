d24126c15f7a436be0b8cd7bee8db1b863b688a0
ranjan1996kumar@gmail.com <ranjan1996kumar@gmail.com>
2021-09-04 22:04:30 +0530

    UC-4- Max Method

---
+++ Parameters.java
@@ -0,0 +1,90 @@
+package com.bl.generic;
+
+import java.util.Scanner;
+
+public class Parameters <T extends Comparable<T>> {
+        //declaring instance variables
+        Integer[] arrayOfInt;
+        Float []  arrayOfFloat;
+        String[] arrayOfString;
+
+        public Parameters(Integer[] arrayOfInt, Float[] arrayOfFloat, String[] arrayOfString) {
+            this.arrayOfInt = arrayOfInt;
+            this.arrayOfFloat = arrayOfFloat;
+            this.arrayOfString = arrayOfString;
+        }
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            // taking input
+            System.out.println("Enter the size of all three arrays : ");
+            int size = scanner.nextInt();
+
+            Integer[] a = new Integer[size];
+            Float[] b = new Float[size];
+            String[] c = new String[size];
+
+            // getting user input for integer
+            System.out.println("Enter " + size + " Integer Values : ");
+            for (int i = 0; i < size; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+            //getting user input for float
+            System.out.println("Enter " + size + " Float Values : ");
+            for (int i = 0; i < size; i++) {
+                b[i] = scanner.nextFloat();
+            }
+
+            // getting user input for string
+            System.out.println("Enter " + size + " String Values : ");
+            for (int i = 0; i < size; i++) {
+                c[i] = scanner.next();
+            }
+
+            new Parameters(a, b, c).testMaximum();
+        }
+
+        private void testMaximum() {
+            //passing instance variable to static method
+            Integer maxOfThreeInt = printMax(arrayOfInt);
+            System.out.println("The maximum value of three integers is : " + maxOfThreeInt);
+
+            Float maxOfThreeFloat = printMax(arrayOfFloat);
+            System.out.println("The maximum value of three floats is : " + maxOfThreeFloat);
+
+            String maxOfThreeString = printMax(arrayOfString);
+            System.out.println("The maximum value of three string is : " + maxOfThreeString);
+        }
+
+        //displays maximum of three values
+        public static Integer printMax(Integer[] someArray) {
+
+            Integer max = someArray[0];
+            for (Integer t : someArray) {
+                if (t.compareTo(max) > 0)
+                    max = t;
+            }
+            return max;
+        }
+        public static Float printMax(Float[] someArray) {
+
+            Float max = someArray[0];
+            for (Float t : someArray) {
+                if (t.compareTo(max) > 0)
+                    max = t;
+            }
+            return max;
+        }
+        public static String printMax(String[] someArray) {
+
+            String max = someArray[0];
+            for (String t : someArray) {
+                if (t.compareTo(max) > 0)
+                    max = t;
+            }
+            return max;
+        }
+}
